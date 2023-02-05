35f72ed395e1ec0cd4eee4b0f588605805266ca3
Man <manbanhduc@gmail.com>
2023-02-05 05:11:19 +0100

    Fix design color and add data website in database docker

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -18,35 +18,35 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[IsGranted("ROLE_ADMIN")]
-    #[Route('/admin', name: 'app_gwada_admin')]
-    public function index(): Response
-    {
-        return $this->render('admin/index.html.twig');
-    }
+  #[IsGranted("ROLE_ADMIN")]
+  #[Route('/admin', name: 'app_gwada_admin')]
+  public function index(): Response
+  {
+    return $this->render('admin/index.html.twig');
+  }
 
-    public function configureDashboard(): Dashboard
-    {
-        return Dashboard::new()
-            ->setTitle('Gwada Excursion')
-            ->renderContentMaximized()
-        ;
-    }
+  public function configureDashboard(): Dashboard
+  {
+    return Dashboard::new()
+      ->setTitle('Gwada Excursion')
+      ->renderContentMaximized();
+  }
 
-    public function configureMenuItems(): iterable
-    {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-bar-chart');
+  public function configureMenuItems(): iterable
+  {
+    yield MenuItem::linkToDashboard('Dashboard', 'fa fa-bar-chart');
 
-        yield MenuItem::subMenu('Website', 'fas fa-bar')
-            ->setSubItems([
-            MenuItem::linkToCrud('Accueil', 'fas fa-home', Home::class),
-            MenuItem::linkToCrud('A propos', 'fas fa-user', About::class),
-            MenuItem::linkToCrud('Activités', 'fas fa-list', Services::class),
-            MenuItem::linkToCrud('Image', 'fas fa-list', Slide::class),
-            MenuItem::linkToCrud('Formule', 'fas fa-file', Formule::class),
-            MenuItem::linkToCrud('Contact', 'fas fa-users', ContactInfo::class),
-        ]);
+    yield MenuItem::subMenu('Website', 'fas fa-bar')
+      ->setSubItems([
+        MenuItem::linkToCrud('Accueil', 'fas fa-home', Home::class),
+        MenuItem::linkToCrud('A propos', 'fas fa-user', About::class),
+        MenuItem::linkToCrud('Activités', 'fas fa-list', Services::class),
+        MenuItem::linkToCrud('Image', 'fas fa-list', Slide::class),
+        MenuItem::linkToCrud('Formule', 'fas fa-file', Formule::class),
+        MenuItem::linkToCrud('Contact', 'fas fa-users', ContactInfo::class),
+      ]);
 
-        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
-    }
+    yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
+    yield MenuItem::linkToUrl('Gwada site', 'fas fa-home', $this->generateUrl('app_home'));
+  }
 }
