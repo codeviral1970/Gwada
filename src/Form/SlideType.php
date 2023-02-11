3a4d28a7ced16f57ce4b101ad3280ef2c28e34f6
Man <manbanhduc@gmail.com>
2023-02-11 10:26:08 +0100

    fix php cs fixer

--- src/Form/SlideType.php
+++ src/Form/SlideType.php
@@ -4,26 +4,26 @@ namespace App\Form;
 
 use App\Entity\Slide;
 use Symfony\Component\Form\AbstractType;
-use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class SlideType extends AbstractType
 {
-  public function buildForm(FormBuilderInterface $builder, array $options): void
-  {
-    $builder
-      ->add('imageFile', VichFileType::class, [
-        'label' => 'Fichier',
-      ])
-      ->add('caption', TextType::class);
-  }
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+          ->add('imageFile', VichFileType::class, [
+            'label' => 'Fichier',
+          ])
+          ->add('caption', TextType::class);
+    }
 
-  public function configureOptions(OptionsResolver $resolver): void
-  {
-    $resolver->setDefaults([
-      'data_class' => Slide::class,
-    ]);
-  }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+          'data_class' => Slide::class,
+        ]);
+    }
 }
