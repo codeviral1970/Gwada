2682fa6e4129fbaaa5cb7cf00f7e5919793456ec
Man <manbanhduc@gmail.com>
2023-02-11 07:41:41 +0100

    Ajout meilleur excursions + fix js + design vue

--- src/Entity/Services.php
+++ src/Entity/Services.php
@@ -19,268 +19,275 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
 #[UniqueEntity('name')]
 class Services
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
+  #[ORM\Column(length: 255)]
+  private ?string $name = null;
 
-    #[ORM\Column(type: Types::TEXT)]
-    #[Assert\NotBlank]
-    private ?string $description = null;
+  #[ORM\Column(type: Types::TEXT)]
+  #[Assert\NotBlank]
+  private ?string $description = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $avatar = null;
+  #[ORM\Column(length: 255, nullable: true)]
+  private ?string $avatar = null;
 
-    #[ORM\Column(length: 255)]
-    #[Slug(fields: ['name'], updatable: true)]
-    private ?string $slug = null;
+  #[ORM\Column(length: 255)]
+  #[Slug(fields: ['name'], updatable: true)]
+  private ?string $slug = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $goodToKnow = null;
+  #[ORM\Column(type: Types::TEXT, nullable: true)]
+  private ?string $goodToKnow = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $courseOfTheDay = null;
+  #[ORM\Column(type: Types::TEXT, nullable: true)]
+  private ?string $courseOfTheDay = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $menu = null;
+  #[ORM\Column(type: Types::TEXT, nullable: true)]
+  private ?string $menu = null;
 
-    #[ORM\OneToMany(mappedBy: 'activity', targetEntity: Formule::class)]
-    private Collection $formules;
+  #[ORM\OneToMany(mappedBy: 'activity', targetEntity: Formule::class)]
+  private Collection $formules;
 
-    // NOTE: This is not a mapped field of entity metadata, just a simple property.
-    #[Vich\UploadableField(mapping: 'services', fileNameProperty: 'avatar', size: 'imageSize')]
-    private ?File $imageFile = null;
+  #[Vich\UploadableField(mapping: 'services', fileNameProperty: 'avatar', size: 'imageSize')]
+  private ?File $imageFile = null;
 
-    #[ORM\Column(type: 'string')]
-    private ?string $imageName = null;
+  #[ORM\Column(type: 'string')]
+  private ?string $imageName = null;
 
-    #[ORM\Column(type: 'integer')]
-    private ?int $imageSize = null;
+  #[ORM\Column(type: 'integer')]
+  private ?int $imageSize = null;
 
-    #[ORM\Column(nullable: true)]
-    private ?\DateTimeImmutable $updatedAt = null;
+  #[ORM\Column(nullable: true)]
+  private ?\DateTimeImmutable $updatedAt = null;
 
-    #[ORM\OneToMany(mappedBy: 'gallery', targetEntity: Slide::class)]
-    private Collection $slides;
+  #[ORM\OneToMany(mappedBy: 'gallery', targetEntity: Slide::class)]
+  private Collection $slides;
 
-    /**
-     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
-     * of 'UploadedFile' is injected into this setter to trigger the update. If this
-     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
-     * must be able to accept an instance of 'File' as the bundle will inject one here
-     * during Doctrine hydration.
-     *
-     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
-     */
-    public function setImageFile(?File $imageFile = null): void
-    {
-        $this->imageFile = $imageFile;
+  #[ORM\Column]
+  private ?bool $isBest = null;
 
-        if (null !== $imageFile) {
-            // It is required that at least one field changes if you are using doctrine
-            // otherwise the event listeners won't be called and the file is lost
-            $this->updatedAt = new \DateTimeImmutable();
-        }
+  /**
+   * 
+   * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+   */
+  public function setImageFile(?File $imageFile = null): void
+  {
+    $this->imageFile = $imageFile;
+
+    if (null !== $imageFile) {
+      $this->updatedAt = new \DateTimeImmutable();
+    }
+  }
+
+  public function getImageFile(): ?File
+  {
+    return $this->imageFile;
+  }
+
+  public function setImageName(?string $imageName): void
+  {
+    $this->imageName = $imageName;
+  }
+
+  public function getImageName(): ?string
+  {
+    return $this->imageName;
+  }
+
+  public function setImageSize(?int $imageSize): void
+  {
+    $this->imageSize = $imageSize;
+  }
+
+  public function getImageSize(): ?int
+  {
+    return $this->imageSize;
+  }
+
+  public function __construct()
+  {
+    return $this->formules = new ArrayCollection();
+    //$this->service = new ArrayCollection();
+    $this->slides = new ArrayCollection();
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getName(): ?string
+  {
+    return $this->name;
+  }
+
+  public function setName(string $name): self
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  public function getDescription(): ?string
+  {
+    return $this->description;
+  }
+
+  public function setDescription(string $description): self
+  {
+    $this->description = $description;
+
+    return $this;
+  }
+
+  public function getAvatar(): ?string
+  {
+    return $this->avatar;
+  }
+
+  public function setAvatar(?string $avatar): self
+  {
+    $this->avatar = $avatar;
+
+    return $this;
+  }
+
+  public function getSlug(): ?string
+  {
+    return $this->slug;
+  }
+
+  public function setSlug(?string $slug): self
+  {
+    $this->slug = $slug;
+
+    return $this;
+  }
+
+  public function getGoodToKnow(): ?string
+  {
+    return $this->goodToKnow;
+  }
+
+  public function setGoodToKnow(?string $goodToKnow): self
+  {
+    $this->goodToKnow = $goodToKnow;
+
+    return $this;
+  }
+
+  public function getCourseOfTheDay(): ?string
+  {
+    return $this->courseOfTheDay;
+  }
+
+  public function setCourseOfTheDay(?string $courseOfTheDay): self
+  {
+    $this->courseOfTheDay = $courseOfTheDay;
+
+    return $this;
+  }
+
+  public function getMenu(): ?string
+  {
+    return $this->menu;
+  }
+
+  public function setMenu(?string $menu): self
+  {
+    $this->menu = $menu;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, Formule>
+   */
+  public function getFormules(): Collection
+  {
+    return $this->formules;
+  }
+
+  public function addFormule(Formule $formule): self
+  {
+    if (!$this->formules->contains($formule)) {
+      $this->formules->add($formule);
+      $formule->setActivity($this);
     }
 
-    public function getImageFile(): ?File
-    {
-        return $this->imageFile;
+    return $this;
+  }
+
+  public function removeFormule(Formule $formule): self
+  {
+    if ($this->formules->removeElement($formule)) {
+      // set the owning side to null (unless already changed)
+      if ($formule->getActivity() === $this) {
+        $formule->setActivity(null);
+      }
     }
 
-    public function setImageName(?string $imageName): void
-    {
-        $this->imageName = $imageName;
+    return $this;
+  }
+
+  public function __toString(): string
+  {
+    return $this->name;
+  }
+
+  public function getUpdatedAt(): ?\DateTimeImmutable
+  {
+    return $this->updatedAt;
+  }
+
+  public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+  {
+    $this->updatedAt = $updatedAt;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, Slide>
+   */
+  public function getSlides(): Collection
+  {
+    return $this->slides;
+  }
+
+  public function addSlide(Slide $slide): self
+  {
+    if (!$this->slides->contains($slide)) {
+      $this->slides->add($slide);
+      $slide->setGallery($this);
     }
 
-    public function getImageName(): ?string
-    {
-        return $this->imageName;
+    return $this;
+  }
+
+  public function removeSlide(Slide $slide): self
+  {
+    if ($this->slides->removeElement($slide)) {
+      // set the owning side to null (unless already changed)
+      if ($slide->getGallery() === $this) {
+        $slide->setGallery(null);
+      }
     }
 
-    public function setImageSize(?int $imageSize): void
-    {
-        $this->imageSize = $imageSize;
-    }
+    return $this;
+  }
 
-    public function getImageSize(): ?int
-    {
-        return $this->imageSize;
-    }
+  public function isIsBest(): ?bool
+  {
+      return $this->isBest;
+  }
 
-    public function __construct()
-    {
-        return $this->formules = new ArrayCollection();
-        $this->service = new ArrayCollection();
-        $this->slides = new ArrayCollection();
-    }
+  public function setIsBest(bool $isBest): self
+  {
+      $this->isBest = $isBest;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getAvatar(): ?string
-    {
-        return $this->avatar;
-    }
-
-    public function setAvatar(?string $avatar): self
-    {
-        $this->avatar = $avatar;
-
-        return $this;
-    }
-
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
-
-    public function setSlug(?string $slug): self
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
-
-    public function getGoodToKnow(): ?string
-    {
-        return $this->goodToKnow;
-    }
-
-    public function setGoodToKnow(?string $goodToKnow): self
-    {
-        $this->goodToKnow = $goodToKnow;
-
-        return $this;
-    }
-
-    public function getCourseOfTheDay(): ?string
-    {
-        return $this->courseOfTheDay;
-    }
-
-    public function setCourseOfTheDay(?string $courseOfTheDay): self
-    {
-        $this->courseOfTheDay = $courseOfTheDay;
-
-        return $this;
-    }
-
-    public function getMenu(): ?string
-    {
-        return $this->menu;
-    }
-
-    public function setMenu(?string $menu): self
-    {
-        $this->menu = $menu;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Formule>
-     */
-    public function getFormules(): Collection
-    {
-        return $this->formules;
-    }
-
-    public function addFormule(Formule $formule): self
-    {
-        if (!$this->formules->contains($formule)) {
-            $this->formules->add($formule);
-            $formule->setActivity($this);
-        }
-
-        return $this;
-    }
-
-    public function removeFormule(Formule $formule): self
-    {
-        if ($this->formules->removeElement($formule)) {
-            // set the owning side to null (unless already changed)
-            if ($formule->getActivity() === $this) {
-                $formule->setActivity(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function __toString(): string
-    {
-        return $this->name;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Slide>
-     */
-    public function getSlides(): Collection
-    {
-        return $this->slides;
-    }
-
-    public function addSlide(Slide $slide): self
-    {
-        if (!$this->slides->contains($slide)) {
-            $this->slides->add($slide);
-            $slide->setGallery($this);
-        }
-
-        return $this;
-    }
-
-    public function removeSlide(Slide $slide): self
-    {
-        if ($this->slides->removeElement($slide)) {
-            // set the owning side to null (unless already changed)
-            if ($slide->getGallery() === $this) {
-                $slide->setGallery(null);
-            }
-        }
-
-        return $this;
-    }
+      return $this;
+  }
 }
