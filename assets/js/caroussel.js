e85a7a8b4532c3cb0e17e845b6c236a6c18a86c0
Man <manbanhduc@gmail.com>
2023-08-15 07:56:25 +0200

    Fix slide to 1 secondes

--- assets/js/caroussel.js
+++ assets/js/caroussel.js
@@ -1,4 +1,25 @@
-// import { Carousel, initTE } from "tw-elements";
+import { Carousel, initTE } from "tw-elements";
 
-// console.log("tw");
-// initTE({ Carousel });
+// const myCarousel = document.getElementById("gwada-slider");
+// myCarousel.addEventListener("slide.te.carousel", () => {
+//
+// });
+
+// const myCarousel = new Carousel(
+//   document.getElementById("gwada-slider"),
+//   (interval = "1000")
+// );
+
+const options = {
+  interval: 1000, // Set the interval to 3000 milliseconds
+  ride: true,
+  wrap: true,
+  touch: true,
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const myCarouselElement = document.getElementById("gwada-slider");
+  const myCarousel = new Carousel(myCarouselElement, options);
+});
+
+initTE({ Carousel });
